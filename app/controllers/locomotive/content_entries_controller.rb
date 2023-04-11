893d5257e1ca6a66b15b31ff1ef04148be9b9b2f
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.34>
2023-04-11 22:33:56 +0700

    New Update

--- app/controllers/locomotive/content_entries_controller.rb
+++ app/controllers/locomotive/content_entries_controller.rb
@@ -65,7 +65,7 @@ module Locomotive
     def update
       authorize @content_entry
       service.update(@content_entry, content_entry_params)
-      Rails.logger.debug("xxxxxxxxx: #{content_entry_params.inspect} #{@content_entry.inspect}")
+      Rails.logger.debug("xxxxxxxxx: #{content_entry_params.inspect} yyy #{@content_entry.inspect}")
 
       respond_with @content_entry, location: -> { location_after_persisting }
     end
@@ -96,6 +96,8 @@ module Locomotive
 
     def load_content_entry
       @content_entry = @content_type.entries.find(params[:id])
+      Rails.logger.debug("zzzzzzzz: #{params[:id]}")
+
     end
 
     def service
