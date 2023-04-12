ba2eb51d2e2102b716e3f124bcf796032cdb5b94
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.34>
2023-04-12 15:27:32 +0700

    New Update

--- app/uploaders/locomotive/editable_file_uploader.rb
+++ app/uploaders/locomotive/editable_file_uploader.rb
@@ -5,10 +5,11 @@ module Locomotive
 
     def store_dir
       self.build_store_dir('sites', model.page.site_id, 'pages', model.page.id, 'files')
+      
     end
 
     def image?
-      ""
+      Rails.logger.info "xxxxxxxxxxxxxxx #{self}"
     end
 
   end
