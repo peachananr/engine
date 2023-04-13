9e27fa99cbe47b286668e794b3df5104cb3d0fd3
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.34>
2023-04-13 20:57:12 +0700

    New Update

--- lib/locomotive/custom_fields.rb
+++ lib/locomotive/custom_fields.rb
@@ -44,6 +44,7 @@ module CustomFields
           #!(self =~ /jpg|png|webp|jpeg|gif/).nil?
           !(content_type =~ /image/).nil?
           puts caller
+          xxx
         rescue Exception => e
           Rails.logger.error("[CustomFields][FileUploader][#{model._id}] can't access the uploaded file, reason: #{e.message}")
         end
